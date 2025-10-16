15f2d9dd2d8fc50c6b7fe08e2d0efacafa19e423
harshavardhan206 <harshabalaga20@gmail.com>
2025-10-16 10:37:39 +0530

    Add all remaining project files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Git Branching & Merge Conflict Resolution</h1>
+      <p>This is the main branch version of App.jsx</p>
+    </div>
+  );
+}
+
+export default App;
